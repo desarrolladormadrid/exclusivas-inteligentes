58a1e45ae1c3aad6975511d95634783947e6275a
desarrolladormadrid <desarrolladormadrid@gmail.com>
2026-08-31 16:27:46 +0200

    fix: poner modales por encima del menu tablet

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import QRCode from "qrcode";
 // @ts-ignore Tipos incluidos por la librería.
 import JsBarcode from "jsbarcode";
 
-const APP_VERSION = "2.0.38";
+const APP_VERSION = "2.0.39";
 const APP_ENVIRONMENT = process.env.NODE_ENV === "production" ? "Producción" : "Local";
 
 const initialModules = [
